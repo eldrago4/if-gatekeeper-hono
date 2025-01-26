e456e5a35825aabd9018f26332d5d8489807246d
eldrago. <tred38434@gmail.com>
2025-01-26 12:31:21 +0530

    routes not showing

--- 1ved-cloud/app/assets/flights.js
+++ 1ved-cloud/app/assets/flights.js
@@ -63,14 +63,14 @@ var Stadia_AlidadeSmoothDark = L.tileLayer(
             '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
         ext: "png",
     },
-);
+).addTo(map);
 
 var osm = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution:
         'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
     setZoom: 7,
     maxZoom: 18,
-}).addTo(map);
+});
 
 var baseMaps = {
     "Open Street Map": osm,
@@ -554,8 +554,11 @@ function addRoute(route) {
     const group = classifyRoute(route);
     if (group === 'group1') {
         routeLayer.addTo(group1Layer);
-    } else {
+    } else if (group === 'group2'){
         routeLayer.addTo(group2Layer);
+    }
+    else {
+        routeLayer.addTo(defaultRoutesLayer);
     }
 
 }
@@ -567,8 +570,8 @@ routes.forEach(route => {
 
 L.control.layers(baseMaps, overlayMaps).addTo(map);
 
-map.on("click", resetHighlight);
-map.on("popupclose", resetHighlight);
+// map.on("click", resetHighlight);
+// map.on("popupclose", resetHighlight);
 map.setZoom(5);
 resetInactivityTimer();
 
