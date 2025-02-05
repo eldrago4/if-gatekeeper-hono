99c6b9525bdec7a8581f54756599d9305d51dd6e
eldrago. <tred38434@gmail.com>
2025-02-05 10:49:59 +0530

    default handler

--- api/index.js
+++ api/index.js
@@ -530,8 +530,9 @@ app.get('/api/simbrief', async (c) => {
 
 const handler = handle(app);
 
-export const GET = handler;
+// export const GET = handler;
 // export const POST = handler;
 // export const PATCH = handler;
 // export const PUT = handler;
-export const OPTIONS = handler;
+// export const OPTIONS = handler;
+export default app;
