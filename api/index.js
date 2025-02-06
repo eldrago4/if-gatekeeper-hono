2bca46a8338da55968bc395180ae1a26b6385433
eldrago. <tred38434@gmail.com>
2025-02-06 12:26:30 +0000

    err

--- api/index.js
+++ api/index.js
@@ -522,6 +522,17 @@ app.get('/api/simbrief', async (c) => {
     }
 });
 
+app.onError(async (err, c) => {
+  try {
+    const html = await readFile(join(__dirname, 'err.html'), 'utf-8');
+    console.error(`${err}`);
+    return c.html(html);
+  } catch (readErr) {
+    console.error('Error reading error page~', readErr);
+    return c.text('Internal Server Error', 500);
+  }
+}); 
+
 const handler = handle(app);
 
 export const GET = handler;
